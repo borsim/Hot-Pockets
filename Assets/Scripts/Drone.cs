4f8af83d2a5a5a3f844f4f7a4eab91f2a3559383
Miklos Borsi <bmiki97@gmail.com>
2020-02-29 14:46:25 +0000

    Fixed drone.cs compilation errors, added UI functionality

--- Assets/Scripts/Drone.cs
+++ Assets/Scripts/Drone.cs
@@ -1,54 +1,56 @@
 using UnityEngine;
-using UnityEngine.Time;
 using System.Collections;
 
 public class Drone : MonoBehaviour
 {
 		private float enginePower = 1.0f;
 		private float maxSpeed = 10.0f;
-		private const Vector3 gravity = new Vector3(0, -9.81f, 0);
-		private const float airResistance = 0.1;
+		private Vector3 gravity = new Vector3(0, -9.81f, 0);
+		private const float airResistance = 0.1f;
 		private const float droneWeight = 1.0f;
 
 
-
+		public bool usesThermals = true;
 		private float powerUsed;
 		private GameObject currentStart;
 		private GameObject currentEnd;
-		private RigidBody rb;
-		private Boolean finished;
+		private Rigidbody rb;
+		private bool finished;
 		//private  PathingScript pathingScript;
+		private UIPowerTracker upt;
 
 
     void Start() {
         powerUsed = 0.0f;
-        rb = gameObject.GetComponent<RigidBody>() as RigidBody;
+        rb = gameObject.GetComponent<Rigidbody>() as Rigidbody;
         finished = false;
+        upt = GameObject.Find("Canvas").GetComponent<UIPowerTracker>() as UIPowerTracker;
         //pathingScript = gameObject.GetComponent<PathingScript>() as PathingScript;
     }
 
     void Update() {
 
-    	Vector3 movementNormal = Vector3.Normalize(new Vector3(currentEnd.transform.position - currentStart.transform.position));
-    	Vector3 positionNormal = Vector3.Normalize(new Vector3(gameObject.transform.position - currentEnd.transform.position));
-    	Boolean nextCube = (movementNormal == positionNormal);
+    	Vector3 movementNormal = Vector3.Normalize(currentEnd.transform.position - currentStart.transform.position);
+    	Vector3 positionNormal = Vector3.Normalize(gameObject.transform.position - currentEnd.transform.position);
+    	bool nextCube = (movementNormal == positionNormal);
     	if (nextCube && !finished) {
     		currentStart = currentEnd;
-    		currentEnd = pathingScript.getNextCube();
-    		if (currentEnd == new Vector3(0, 0, 0)) finished = true;
+    		//currentEnd = pathingScript.getNextCube();
+    		if (currentEnd.transform.position == new Vector3(0, 0, 0)) finished = true;
   		} 
   		// otherwise move in direction
   		else if (!finished){
   			// assuming: constant (drone maximum speed) speed
   			rb.velocity = movementNormal * maxSpeed;
   			// calculate air drag
-  			dragForce = rb.velocity * airResistance;
+  			Vector3 dragForce = rb.velocity * airResistance;
   			// calculcate gravity
-  			gravityForce = gravity * droneWeight;
+  			Vector3 gravityForce = gravity * droneWeight;
   			// sum vectors, calculate thrust
-  			enginePowerExpended = dragForce + gravityForce + localWind() + localThermal();
+  			Vector3 enginePowerExpended = dragForce + gravityForce + localWind() + localThermal();
 
-  			powerUsed = Time.deltaTime() * enginePowerExpended.magnitude;
+  			powerUsed = Time.deltaTime * enginePowerExpended.magnitude;
+  			upt.submitPowerUsage(powerUsed, usesThermals);
   			// rotate drone to show off
   			gameObject.transform.eulerAngles = -enginePowerExpended;
 
@@ -56,10 +58,10 @@ public class Drone : MonoBehaviour
     }
 
     Vector3 localWind() {
-
+    	return new Vector3(1,1,1);
     }
     Vector3 localThermal() {
-
+    	return new Vector3(1,1,1);
     }
 
 }
---
+++ Assets/Scripts/UIPowerTracker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPowerTracker : MonoBehaviour {
+
+	private Text WOPowerNumber;
+	private Text WTPowerNumber;
+	private RectTransform WOPowerBar;
+	private RectTransform WTPowerBar;
+
+
+	private const float MAX_POWER_NUMBER = 500.0f;
+	private const float MAX_BAR_HEIGHT = 200.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		WTPowerBar = (RectTransform)(GameObject.Find("TrackerFillerWT").GetComponent(typeof(Image)) as Image).transform;
+		WOPowerBar = (RectTransform)(GameObject.Find("TrackerFillerWO").GetComponent(typeof(Image)) as Image).transform;
+		WTPowerNumber = GameObject.Find("PowerWTNumber").GetComponent<Text>() as Text;
+		WOPowerNumber = GameObject.Find("PowerWONumber").GetComponent<Text>() as Text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void submitPowerUsage(float powerUsedThisRun, bool thermals) {
+		if (thermals) {
+			WTPowerNumber.text = (Mathf.Round(powerUsedThisRun)).ToString();
+			WTPowerBar.sizeDelta = new Vector2(50f, (powerUsedThisRun / MAX_POWER_NUMBER) * MAX_BAR_HEIGHT);
+		} else {
+			WOPowerNumber.text = (Mathf.Round(powerUsedThisRun)).ToString();
+			WOPowerBar.sizeDelta = new Vector2(50f, (powerUsedThisRun / MAX_POWER_NUMBER) * MAX_BAR_HEIGHT);
+		}
+	}
+
+}
