2e1eba9ca7013381d94acdd684a7c47419930f21
electon100 <cf16670@my.bristol.ac.uk>
2020-02-29 12:57:16 +0000

    Dynamic cube instantiation

--- Assets/Scripts/Weather.cs
+++ Assets/Scripts/Weather.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Weather : MonoBehaviour
 {
+    public GameObject cube;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(cube, new Vector3(0, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
