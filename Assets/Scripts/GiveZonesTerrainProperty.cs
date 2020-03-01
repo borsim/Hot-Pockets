f8f2bdd98926c7154efc46a6a1fceef78d1c4552
Joe <Joseph.Mills22@gmail.com>
2020-03-01 10:27:18 +0000

    Added tower prefab, 15x15x15 grid no map

--- Assets/Scripts/GiveZonesTerrainProperty.cs
+++ Assets/Scripts/GiveZonesTerrainProperty.cs
@@ -15,12 +15,12 @@ public class GiveZonesTerrainProperty : MonoBehaviour
 
     private void OnTriggerEnter(Collider collision)
     {
-        Debug.Log("Collision!!");
+        //Debug.Log("Collision!!");
         Zone collidedZone = collision.gameObject.GetComponent<Zone>() as Zone;
         if (collidedZone)
         {
             collidedZone.terrain = true;
-            Debug.Log("Terrain!");
+            //Debug.Log("Terrain!");
         }
     }
 }
